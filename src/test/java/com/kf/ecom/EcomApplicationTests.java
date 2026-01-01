6f065a180ebc9d64128caeeda67a106f7aa0e215
@karthikeyan171202 <karthikeyan.r@thoughtmakes.com>
2026-01-02 01:56:17 +0530

    Initial Spring Boot project

---
+++ src/test/java/com/kf/ecom/EcomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kf.ecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
